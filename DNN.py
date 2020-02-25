4ed7255cdb05e7d7154b4f90fe2a02123048b1eb
tachibana <tachibana.3334@gmail.com>
2020-02-25 16:26:50 +0900

    fix s

--- DNN.py
+++ DNN.py
@@ -25,32 +25,38 @@ Y = data[x_size:].T
 
 #スペクトルデータを正規化(zscore)
 X = scipy.stats.zscore(X)
-
+#X = X.astype('float32')/1500
 #学習データとテストデータに分割
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y,train_size=0.8)
 
 #Yラベル(ペプチドアルファベット26個＋emptyの数字表現)をカテゴリ変数に変換
 #大きさ　27*20
 num_classes = 27
-y_train = to_categorical(Y_train, num_classes = num_classes)
-y_test = to_categorical(Y_test, num_classes = num_classes)
-y_train=y_train.reshape((-1,20*27))
-y_test=y_test.reshape((-1,20*27))
+seq_len =20
+Y_train = Y_train[:,:seq_len]
+Y_test = Y_test[:,:seq_len]
+Y_train = to_categorical(Y_train, num_classes = num_classes)
+Y_test = to_categorical(Y_test, num_classes = num_classes)
+Y_train=Y_train.reshape((-1,seq_len*num_classes))
+Y_test=Y_test.reshape((-1,seq_len*num_classes))
 
 #隠れ層二層モデル構築
 
 model = Sequential()
 
-model.add(Dense(500, input_dim=100))
+model.add(Dense(200, input_dim=100))
 model.add(Activation("relu"))
-model.add(Dropout(rate=0.2))
+model.add(Dropout(rate=0.4))
 
-model.add(Dense(100))
+model.add(Dense(80))
 model.add(Activation("relu"))
-model.add(Dropout(rate=0.5))
+model.add(Dropout(rate=0.4))
 
-model.add(Dense(20*num_classes))
-model.add(Activation("softmax"))
+
+
+
+model.add(Dense(seq_len*num_classes))
+#model.add(Activation("softmax"))
 
 #最適化関数 learning rate = 0.001
 optimizer = Adam(lr=0.001)
@@ -92,9 +98,9 @@ logging = TensorBoard(log_dir="log/")
 #学習
 hist = model.fit(
     X_train,
-    y_train,
+    Y_train,
     verbose=1,
-    epochs=50,
+    epochs=2000,
     batch_size = 32,
     validation_split=0.2,
     callbacks=[ reduce_lr, logging]
@@ -133,6 +139,6 @@ plt.legend(loc="best")
 plt.grid(color = 'gray', alpha = 0.2)
 
 plt.savefig(now+"train_curve.png")
-score = model.evaluate(X_test, y_test, verbose=1)
+score = model.evaluate(X_test, Y_test, verbose=1)
 print("evaluate loss: {0[0]}".format(score))
 print("evaluate acc: {0[1]}".format(score))
